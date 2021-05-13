0da6804cfd42e73416d18973e1f6d2b2a0249f27
Luke <lochoda@endai.com>
2021-05-13 12:37:55 -0400

    Update run-vsftpd.sh

--- run-vsftpd.sh
+++ run-vsftpd.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
 
-# If no env var for FTP_USER has been specified, use 'admin':
-if [ "$FTP_USER" = "**String**" ]; then
-    export FTP_USER='admin'
-fi
-
-# If no env var has been specified, generate a random password for FTP_USER:
-if [ "$FTP_PASS" = "**Random**" ]; then
-    export FTP_PASS=`cat /dev/urandom | tr -dc A-Z-a-z-0-9 | head -c${1:-16}`
-fi
 
 # Do not log to STDOUT by default:
 if [ "$LOG_STDOUT" = "**Boolean**" ]; then
@@ -18,10 +9,8 @@ else
 fi
 
 # Create home dir and update vsftpd user db:
-mkdir -p "/home/vsftpd/${FTP_USER}"
 chown -R ftp:ftp /home/vsftpd/
 
-echo -e "${FTP_USER}\n${FTP_PASS}" > /etc/vsftpd/virtual_users.txt
 /usr/bin/db_load -T -t hash -f /etc/vsftpd/virtual_users.txt /etc/vsftpd/virtual_users.db
 
 # Set passive mode parameters:
@@ -56,8 +45,6 @@ cat << EOB
 
 	SERVER SETTINGS
 	---------------
-	· FTP User: $FTP_USER
-	· FTP Password: $FTP_PASS
 	· Log file: $LOG_FILE
 	· Redirect vsftpd log to STDOUT: No.
 EOB
